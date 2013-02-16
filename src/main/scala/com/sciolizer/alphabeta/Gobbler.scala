621fc049aea440e4f0ccf6ab9751a0c46ee3f382
Joshua Ball <JoshBBall@gmail.com>
2013-02-16 09:31:07 -0800

    Added Gobbler game

---
+++ src/main/scala/com/sciolizer/alphabeta/Gobbler.scala
@@ -0,0 +1,125 @@
+package com.sciolizer.alphabeta
+
+import aima.core.search.adversarial.{AlphaBetaSearch, Game}
+import java.util
+import collection.JavaConversions._
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jball
+ * Date: 2/16/13
+ * Time: 8:40 AM
+ */
+class Gobbler {
+  case class Piece(player: Int /* 0 or 1 */, size: Int /* 0-smallest, 1, or 2-largest */, which: Int /* 0 or 1 */)
+  object Piece {
+    lazy val allFirst: List[Piece] = (for (s <- 0 until 3; w <- 0 until 2) yield Piece(0, s, w)).toList
+    lazy val allSecond: List[Piece] = (for (s <- 0 until 3; w <- 0 until 2) yield Piece(1, s, w)).toList
+  }
+  case class Square(pieces: List[Piece] /* from largest to smallest */)
+  case class State(board: Map[(Int, Int), Square] /* indices from (0, 0) to (2, 2) */, nextPlayer: Int)
+  case class Action(piece: Piece, where: (Int, Int))
+  object Action {
+    lazy val allCoordinates: List[(Int, Int)] = (for (r <- 0 until 3; c <- 0 until 3) yield (r, c)).toList
+    lazy val allFirst: List[Action] = (for (p <- Piece.allFirst; c <- allCoordinates) yield Action(p, c)).toList
+    lazy val allSecond: List[Action] = (for (p <- Piece.allSecond; c <- allCoordinates) yield Action(p, c)).toList
+  }
+
+  class GobblerGame extends Game[State, Action, Int /* 0 or 1 */] {
+    def getInitialState: State =
+      State(Action.allCoordinates.map(x => x -> Square(List.empty)).toMap, 0)
+
+    def getPlayers: Array[Int] = Array(0, 1)
+
+    def getPlayer(state: State): Int = state.nextPlayer
+
+    def getActions(state: State): util.List[Action] = {
+      val actions = if (state.nextPlayer == 0) Action.allFirst else Action.allSecond
+      (for (a <- actions; Some(_) <- List(getResultOption(state, a))) yield a)
+    }
+
+    def getResult(state: State, action: Action): State = getResultOption(state, action).get
+
+    private def getResultOption(state: State, action: Action): Option[State] = {
+      if (action.piece.player != state.nextPlayer) return None
+      val target = action.where
+      var board = state.board
+      for (coordinate <- Action.allCoordinates) {
+        val square = state.board(coordinate)
+        val pieces = square.pieces
+        if (coordinate == target) {
+          if (pieces.contains(action.piece)) return None
+          board = board.updated(coordinate, Square(action.piece +: pieces))
+        } else {
+          if (pieces.contains(action.piece)) {
+            if (pieces.head == action.piece) {
+              board = board.updated(coordinate, Square(pieces.tail))
+            } else {
+              return None
+            }
+          }
+        }
+      }
+      Some(State(board, 1 - state.nextPlayer))
+    }
+
+    def isTerminal(state: State): Boolean = {
+      winner(state) match {
+        case (false, false) => false
+        case _ => true
+      }
+    }
+
+    def getUtility(state: State, player: Int): Double = {
+      winner(state) match {
+        case (false, false) => throw new IllegalArgumentException("game is not terminal")
+        case (true, false) => if (player == 0) 1 else -1
+        case (false, true) => if (player == 1) 1 else -1
+        case (true, true) => 0
+      }
+    }
+
+    private def winner(state: State): (Boolean, Boolean) = {
+      var ret = List(false, false)
+      def check(coords: List[(Int, Int)]) {
+        player(state, coords) match {
+          case None =>
+          case Some(x) => ret = ret.updated(x, true)
+        }
+      }
+      for (row <- 0 until 3) check(List((row, 0), (row, 1), (row, 2)))
+      for (col <- 0 until 3) check(List((0, col), (1, col), (2, col)))
+      check(List((0, 0), (1, 1), (2, 2)))
+      check(List((0, 2), (1, 1), (2, 0)))
+      (ret(0), ret(1))
+    }
+
+    private def player(state: State, cs: List[(Int, Int)]): Option[Int] = {
+      var ret: Option[Int] = None
+      for (c <- cs) {
+        val pieces = state.board(c).pieces
+        if (pieces.isEmpty) return None
+        val player = pieces.head.player
+        if (ret == Some(1 - player)) {
+          return None
+        }
+        ret = Some(player)
+      }
+      ret
+    }
+  }
+
+  def go() {
+    val gg = new GobblerGame()
+    val ab = AlphaBetaSearch.createFor(gg)
+    println(ab.makeDecision(gg.getInitialState))
+  }
+}
+
+object Gobbler {
+  def main(args: Array[String]) {
+    val g = new Gobbler()
+    g.go()
+  }
+}
+
